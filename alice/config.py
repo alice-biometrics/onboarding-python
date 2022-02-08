c4328e49b817caf32202c9775eaa5434783a87b5
Francisco Rodeño <frodeno@alicebiometrics.com>
2022-02-08 16:19:46 +0100

    FIX. Remove wrong inheritance in config class

--- alice/config.py
+++ alice/config.py
@@ -1,11 +1,10 @@
 from typing import Optional
 
-from meiga import Error
 from pydantic.dataclasses import dataclass
 
 
 @dataclass
-class Config(Error):
+class Config:
     onboarding_url: str = "https://apis.alicebiometrics.com/onboarding"
     sandbox_url: str = "https://apis.alicebiometrics.com/onboarding/sandbox"
     api_key: str = None
