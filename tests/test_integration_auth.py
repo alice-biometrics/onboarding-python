a8a314ffcd79e07be40512be4dfa9026f3b70d3f
acostapazo <acosta@alicebiometrics.com>
2022-11-23 11:19:29 +0100

    fix(testing): fix auth integration testing

--- tests/test_integration_auth.py
+++ tests/test_integration_auth.py
@@ -1,44 +1,51 @@
 import pytest
 
 from alice import Auth, Config
+from alice.auth.auth_errors import AuthError
+from alice.onboarding.onboarding import Onboarding
 
 
 @pytest.mark.unit
-def test_should_return_an_error_when_the_api_key_is_not_configured():
-    config = Config()
-    auth = Auth.from_config(config)
+class TestAuth:
+    user_id: str
 
-    result = auth.create_backend_token()
-    result.assert_failure()
+    def setup_method(self):
+        self.user_id = "85c38a91-e4f8-4d51-b3b5-2db7dc98ada0"
 
+    def _get_user_id(self, config: Config) -> str:
+        onboarding = Onboarding.from_config(config)
+        return onboarding.create_user().unwrap_or_throw()
 
-@pytest.mark.unit
-def test_should_create_a_valid_backend_token(given_valid_api_key):
+    def should_return_an_error_when_the_api_key_is_not_configured(self):
+        config = Config()
+        auth = Auth.from_config(config)
 
-    config = Config(api_key=given_valid_api_key)
-    auth = Auth.from_config(config)
+        result = auth.create_backend_token()
+        result.assert_failure(value_is_instance_of=AuthError)
 
-    backend_token = auth.create_backend_token()
+    def should_create_a_valid_backend_token(self, given_valid_api_key):
 
-    assert backend_token is not None
+        config = Config(api_key=given_valid_api_key)
+        auth = Auth.from_config(config)
 
+        result = auth.create_backend_token()
+        result.assert_success(value_is_instance_of=str)
 
-@pytest.mark.unit
-def test_should_create_a_valid_backend_token_with_user(given_valid_api_key):
+    def should_create_a_valid_backend_token_with_user(self, given_valid_api_key):
 
-    config = Config(api_key=given_valid_api_key)
-    auth = Auth.from_config(config)
+        config = Config(api_key=given_valid_api_key)
+        auth = Auth.from_config(config)
 
-    backend_token_with_user = auth.create_backend_token(user_id="user_id")
+        result = auth.create_backend_token(user_id=self._get_user_id(config))
+        result.assert_success(value_is_instance_of=str)
 
-    assert backend_token_with_user is not None
+    def should_create_a_valid_user_token(self, given_valid_api_key):
 
+        config = Config(api_key=given_valid_api_key)
+        auth = Auth.from_config(config)
 
-@pytest.mark.unit
-def test_should_create_a_valid_user_token(given_valid_api_key):
+        onboarding = Onboarding.from_config(config)
+        user_id = onboarding.create_user().unwrap_or_throw()
 
-    config = Config(api_key=given_valid_api_key)
-    auth = Auth.from_config(config)
-
-    result = auth.create_user_token(user_id="user_id")
-    result.assert_success(value_is_instance_of=str)
+        result = auth.create_user_token(user_id=self._get_user_id(config))
+        result.assert_success(value_is_instance_of=str)
